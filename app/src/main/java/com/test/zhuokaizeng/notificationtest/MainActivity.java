b1aa099226a6cfee348c496d7dbdf507c3491cfb
zhuokai.zeng <zhuokai.zeng@nio.com>
2019-07-17 16:51:01 +0800

    项目初始化

---
+++ app/src/main/java/com/test/zhuokaizeng/notificationtest/MainActivity.java
@@ -0,0 +1,142 @@
+package com.test.zhuokaizeng.notificationtest;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnAdd;
+    private Button mBtnRemove;
+    private Button mBtnClear;
+    public final static int MESSAGE_ADD=1;
+    public final static int MESSAGE_CLEAR=2;
+    public final static int MESSAGE_DELETE_BY_ID=3;
+    public final static String CHANNEL_ID="100";
+    private MyHandler myHandler;
+    public static int mNotificationId=0;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findView();
+        setListener();
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void init() {
+        myHandler=new MyHandler(this);
+    }
+
+    private void setListener() {
+        mBtnClear.setOnClickListener(this);
+        mBtnRemove.setOnClickListener(this);
+        mBtnAdd.setOnClickListener(this);
+    }
+
+    private void findView() {
+        mBtnAdd=findViewById(R.id.btn_add);
+        mBtnRemove=findViewById(R.id.btn_remove);
+        mBtnClear=findViewById(R.id.btn_clear);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_add:
+                myHandler.sendEmptyMessage(MESSAGE_ADD);
+                break;
+            case R.id.btn_remove:
+                myHandler.sendEmptyMessage(MESSAGE_DELETE_BY_ID);
+                break;
+            case R.id.btn_clear:
+                myHandler.sendEmptyMessage(MESSAGE_CLEAR);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static class MyHandler extends Handler{
+
+        private final WeakReference<Activity>mActivity;
+        private NotificationManager mNotificationManager;
+        private Intent mIntent;
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        private MyHandler(Activity activity){
+            mActivity=new WeakReference<>(activity);
+            init(activity);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        private void init(Activity activity) {
+            mNotificationManager = (NotificationManager) activity.getSystemService(NOTIFICATION_SERVICE);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,"my_channel",NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableLights(true); //是否在桌面icon右上角展示小红点
+            channel.setLightColor(Color.GREEN); //小红点颜色
+            channel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
+            mNotificationManager.createNotificationChannel(channel);//创建Channel
+            mIntent=new Intent();
+            mIntent.setClass(activity,SecondActivity.class);
+        }
+
+        @TargetApi(Build.VERSION_CODES.O)
+        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+        @Override
+        public void handleMessage(Message msg) {
+            Activity activity=mActivity.get();
+            if(activity==null){
+                throw new NullPointerException("activity 为空");
+            }else if(msg.what==MESSAGE_ADD){
+                Notification.Builder builder=new Notification.Builder(activity);
+
+                PendingIntent intent=PendingIntent.getActivity(activity,0,mIntent,0);
+
+                builder.setContentTitle("测试  "+mNotificationId) //必须提供
+                        .setContentText("测试内容") //必须提供
+                        .setTicker("测试通知到达")
+                        .setWhen(System.currentTimeMillis())
+                        .setPriority(Notification.PRIORITY_DEFAULT)//设置该通知优先级
+                        .setSmallIcon(R.mipmap.ic_launcher_round) //必须提供
+                        .setOngoing(true)           //  设置常驻用户无法清除
+                        .setContentIntent(intent)   //用于点击通知跳转界面
+                        .setChannelId(CHANNEL_ID); //Android O以上版本必须提供
+
+                mNotificationManager.notify(mNotificationId++, builder.build());
+            }else if(msg.what==MESSAGE_CLEAR){
+                mNotificationManager.cancelAll();
+            }else if(msg.what==MESSAGE_DELETE_BY_ID){
+                if(mNotificationId>0){
+                    mNotificationManager.cancel(mNotificationId-1);
+                    mNotificationId--;
+                }else{
+                    Toast.makeText(activity, "已经没有通知需要清除了", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            super.handleMessage(msg);
+        }
+    }
+}
